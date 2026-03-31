2d528a62cbf683221a2e4c7d23e77da408d0e9fd
Diego Andres <dasalazarr@users.noreply.github.com>
2026-03-31 21:29:42 +0200

    nuevo evento

--- src/components/HomeEvents.tsx
+++ src/components/HomeEvents.tsx
@@ -109,7 +109,7 @@ const HomeEvents = () => {
                                     </span>
                                 </div>
                                 {/* Image */}
-                                <div className="relative h-64 lg:h-auto min-h-[280px] overflow-hidden">
+                                <div className="relative aspect-video overflow-hidden">
                                     <img
                                         src={getLocalizedContent(upcomingEvent).heroImage}
                                         alt={getLocalizedContent(upcomingEvent).title}
